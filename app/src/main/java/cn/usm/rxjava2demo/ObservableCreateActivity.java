89f1ecaf1e52e8b9ba725101286313d0449689c5
softdev4tong <softdev4tong@163.com>
2017-08-07 18:01:49 +0800

    操作符练习

--- app/src/main/java/cn/usm/rxjava2demo/ObservableCreateActivity.java
+++ app/src/main/java/cn/usm/rxjava2demo/ObservableCreateActivity.java
@@ -5,10 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
+import java.util.concurrent.Callable;
+
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
+import io.reactivex.Single;
+import io.reactivex.SingleObserver;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
 
@@ -22,7 +27,7 @@ public class ObservableCreateActivity extends AppCompatActivity {
         setContentView(R.layout.activity_observable_create);
     }
 
-    public void observableClick(View view){
+    public void observableClick(View view) {
         Observable.create(new ObservableOnSubscribe<Integer>() {
             @Override
             public void subscribe(@NonNull ObservableEmitter<Integer> emitter) throws Exception {
@@ -61,5 +66,70 @@ public class ObservableCreateActivity extends AppCompatActivity {
                 Log.d(TAG, "onComplete");
             }
         });
+    }
+
+    public void single(View view) {
+        Single.just(1).subscribe(new SingleObserver<Integer>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                Log.d(TAG, "onSubscribe");
+            }
+
+            @Override
+            public void onSuccess(@NonNull Integer integer) {
+                Log.d(TAG, "onSuccess : " + integer);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Log.d(TAG, "onError");
+            }
+        });
+    }
+
+
+
+    /**
+     * 简单地时候就是每次订阅都会创建一个新的 Observable，并且如果没有被订阅，就不会产生新的 Observable
+     * @param view
+     */
+    boolean flag = true;
+    public void defer(View view) {
+
+        Observable observable = Observable.defer(new Callable<ObservableSource<Integer>>() {
+            @Override
+            public ObservableSource<Integer> call() throws Exception {
+                if (flag) {
+                    flag = !flag;
+                    return Observable.just(1, 2, 3, 4, 5);
+                } else {
+                    flag = !flag;
+                    return Observable.just(6, 7, 8, 9, 0);
+                }
+
+            }
+        });
+        observable.subscribe(new Observer<Integer>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(@NonNull Integer integer) {
+                Log.e(TAG, "defer : " + integer);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Log.e(TAG, "defer : onError : " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.e(TAG, "defer : onComplete");
+            }
+        });
+
     }
 }
--- app/src/main/java/cn/usm/rxjava2demo/OperatorActivity.java
+++ app/src/main/java/cn/usm/rxjava2demo/OperatorActivity.java
@@ -5,7 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -14,11 +16,13 @@ import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.BiFunction;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
 
 public class OperatorActivity extends AppCompatActivity {
@@ -170,5 +174,296 @@ public class OperatorActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    /**
+     * 安顺序发射  ob1  ob2
+     *
+     * @param view
+     */
+    public void concat(View view) {
+
+      /*  Observable observable1 = Observable.create(new ObservableOnSubscribe() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter e) throws Exception {
+                e.onNext(1);
+              //  Thread.sleep(500);
+                e.onNext(2);
+               // Thread.sleep(500);
+                e.onNext(3);
+              //  Thread.sleep(500);
+            }
+        });*/
+
+        Observable.concat(Observable.just(1, 2, 3), Observable.just(4, 5, 6)).subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer i) throws Exception {
+                Log.d(TAG, "concat " + i);
+            }
+        });
+    }
+
+    /**
+     * 去重操作符
+     *
+     * @param view
+     */
+    public void distinct(View view) {
+        Observable.just(1, 2, 3, 3, 3, 4, 5, 6, 8, 8).distinct().subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Exception {
+                Log.d(TAG, "distinct :" + integer);
+            }
+        });
+    }
+
+    /**
+     * 过滤操作符
+     *
+     * @param view
+     */
+    public void filter(View view) {
+        Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
+                .filter(new Predicate<Integer>() {
+                    @Override
+                    public boolean test(@NonNull Integer integer) throws Exception {
+                        //符合条件
+                        return integer >= 5;
+                    }
+                }).subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Exception {
+                Log.d(TAG, "filter :" + integer);
+            }
+        });
+    }
+
+    public void buffer(View view) {
+        Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
+                .buffer(3, 1)
+                .subscribe(new Consumer<List<Integer>>() {
+                    @Override
+                    public void accept(List<Integer> integers) throws Exception {
+                        Log.d(TAG, "buffer size" + integers.size());
+                        for (Integer i : integers) {
+                            Log.d(TAG, "buffer :" + i);
+                        }
+                    }
+                });
+    }
+
+    public void timer(View view) {
+        final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        Log.d(TAG, "click timer start:" + sdf.format(new Date(System.currentTimeMillis())));
+        Observable.timer(2, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Long>() {
+                    @Override
+                    public void accept(Long aLong) throws Exception {
+                        //延迟 发射事件
+                        Log.d(TAG, "click timer end:" + sdf.format(new Date(System.currentTimeMillis())));
+                    }
+                });
+    }
+
+    public void interval(View view) {
+        final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        Log.d(TAG, "click interval start:" + sdf.format(new Date(System.currentTimeMillis())));
+        Observable.interval(2, 3, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Long>() {
+                    @Override
+                    public void accept(Long aLong) throws Exception {
+                        //延迟 发射事件 ,间隔多久发送事件
+                        Log.d(TAG, "click interval end:" + sdf.format(new Date(System.currentTimeMillis())));
+                    }
+                });
+    }
+
+    public void doOnNext(View view) {
+        Observable.just(1, 2, 3, 4, 5)
+                .doOnNext(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        Log.d(TAG, "doOnNext :" + integer);
+                    }
+                }).subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Exception {
+                Log.d(TAG, "subscribe :" + integer);
+            }
+        });
+    }
+
+    public void skip(View view) {
+        Observable.just(1, 2, 3, 4, 5)
+                .skip(2)
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        Log.d(TAG, "skip 2 之后 输出 :" + integer);
+                    }
+                });
+    }
+
+    public void take(View view) {
+        Observable.just(1, 2, 3, 4, 5)
+                .take(3)
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        Log.d(TAG, "只拿3个数据 :" + integer);
+                    }
+                });
+    }
+
+    /**
+     * 去除发送频率过快的项，看起来好像没啥用处，但你信我，后面绝对有地方很有用武之地。
+     *
+     * @param view
+     */
+    public void debounce(View view) {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Integer> emitter) throws Exception {
+                emitter.onNext(1); // skip
+                Thread.sleep(400);
+                emitter.onNext(2); // deliver
+                Thread.sleep(505);
+                emitter.onNext(3); // skip
+                Thread.sleep(100);
+                emitter.onNext(4); // deliver
+                Thread.sleep(605);
+                emitter.onNext(5); // deliver
+                Thread.sleep(510);
+                emitter.onComplete();
+
+            }
+        }).debounce(500, TimeUnit.MILLISECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        Log.e(TAG, "debounce :" + integer);
+                    }
+                });
+    }
+
+    /**
+     * last 操作符仅取出可观察到的最后一个值，或者是满足某些条件的最后一项
+     *
+     * @param view
+     */
+    public void last(View view) {
+        Observable.just(1, 2, 3, 4, 5)
+                .last(4)
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        Log.d(TAG, "last :" + integer);
+                    }
+                });
+    }
+
+    /**
+     * merge 顾名思义，熟悉版本控制工具的你一定不会不知道 merge 命令，
+     * 而在 Rx 操作符中，merge 的作用是把多个 Observable 结合起来，接受可变参数，也支持迭代器集合。
+     * 注意它和 concat 的区别在于，不用等到 发射器 A 发送完所有的事件再进行发射器 B 的发送。
+     *
+     * @param view
+     */
+    public void merge(View view) {
+
+        Observable observable1 = Observable.create(new ObservableOnSubscribe() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter e) throws Exception {
+                e.onNext(1);
+                Thread.sleep(500);
+                e.onNext(2);
+                Thread.sleep(500);
+                e.onNext(3);
+                Thread.sleep(500);
+            }
+        }).subscribeOn(Schedulers.io());
+
+        Observable.merge(observable1, Observable.just(4, 5, 6).subscribeOn(Schedulers.io()))
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(Integer integer) throws Exception {
+                        Log.d(TAG, "merge :" + integer);
+                    }
+                });
+    }
+
+    /**
+     * V
+     * reduce 操作符每次用一个方法处理一个值，可以有一个 seed 作为初始值
+     *
+     * @param view
+     */
+    public void reduce(View view) {
+        Observable.just(1, 2, 3)
+                .reduce(new BiFunction<Integer, Integer, Integer>() {
+                    @Override
+                    public Integer apply(@NonNull Integer integer, @NonNull Integer integer2) throws Exception {
+                        return integer + integer2;
+                    }
+                }).subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Exception {
+                Log.d(TAG, "reduce " + integer);
+            }
+        });
+    }
+
+    /**
+     * scan 操作符作用和上面的 reduce 一致，唯一区别是 reduce 是个只追求结果的坏人，而  scan 会始终如一地把每一个步骤都输出
+     *
+     * @param view
+     */
+    public void scan(View view) {
+        Observable.just(1, 2, 3)
+                .scan(new BiFunction<Integer, Integer, Integer>() {
+                    @Override
+                    public Integer apply(@NonNull Integer integer, @NonNull Integer integer2) throws Exception {
+                        return integer + integer2;
+                    }
+                }).subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Exception {
+                Log.d(TAG, "reduce " + integer);
+            }
+        });
+    }
+
+    /**
+     * 按照实际划分窗口，将数据发送给不同的 Observable
+     * @param view
+     */
+    public void window(View view) {
+        Observable.interval(1,TimeUnit.SECONDS)
+                .take(15)
+                .window(3,TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Observable<Long>>() {
+                    @Override
+                    public void accept(Observable<Long> longObservable) throws Exception {
+                        Log.e(TAG, "Sub Divide begin...");
+                        longObservable.subscribeOn(Schedulers.io())
+                                .observeOn(AndroidSchedulers.mainThread())
+                                .subscribe(new Consumer<Long>() {
+                                    @Override
+                                    public void accept(Long aLong) throws Exception {
+                                        Log.e(TAG, "window: " + aLong );
+                                    }
+                                });
+                    }
+                });
     }
 }
